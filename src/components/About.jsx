b2f6213b91f818549576b5665620ed3cda94b0f8
pratyush Singha <pratyushsingha83@gmail.com>
2023-06-14 07:33:22 +0000

    pratysh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,31 +32,7 @@ const About = () => {
                             We are a dynamic and creative team of professionals with a passion for innovation and a deep understanding of technology. With diverse backgrounds and expertise, we bring a unique perspective to each project we work on.
                         </p>
                     </div>
-                    <div className="flex flex-wrap -m-4">
-                        <div className="p-4 lg:w-1/2">
-                            
-                                                viewBox="0 0 24 24"
-                                    
-                                               
-                                               
-                                        <a className="ml-2 text-gray-500" href='https://www.instagram.com/_i_am_pratyush04/' target='_blank'>
-                                            <svg
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={2}
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                                            </svg>
-                                        </a>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </section>
             <Footer />
